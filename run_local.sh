eef6887854ecf676d69352577e99da20d89088c5
kaium123 <mohammadkaiom79@gmail.com>
2024-11-17 03:06:04 +0600

    change consol key value

--- run_local.sh
+++ run_local.sh
@@ -2,17 +2,15 @@
 
 echo "starting server"
 
-export ME_CONSUL_PATH="me"
+export ME_CONSUL_PATH="order"
 export ME_CONSUL_URL="localhost:8500"
-export GOOGLE_APPLICATION_CREDENTIALS="/$HOME/techetron-service-acc.json"
 
 #export
 
 curl \
     --request PUT \
     --data-binary @config.yaml \
-    http://127.0.0.1:8500/v1/kv/me
+    http://127.0.0.1:8500/v1/kv/order
 
 go run cmd/*.go serve
-#go run cmd/*.go listen_account_creation
 
